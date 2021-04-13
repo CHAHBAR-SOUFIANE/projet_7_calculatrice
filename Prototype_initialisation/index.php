f3ae56f0d450a3939cf16836d8540c73989e8bcc
CHAHBAR-SOUFIANE <chahbar.soufiane.dev@gmail.com>
2021-04-13 13:43:12 +0000

    .

--- Prototype_initialisation/index.php
+++ Prototype_initialisation/index.php
@@ -1,4 +1,14 @@
 <?php 
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+   if (isset($_POST)) {
+       foreach ($_POST as $key => $value) {
+          if (is_numeric($key)) {
+              echo $key;
+          }
+       }
+   }
+
+}
 
 ?>
 
@@ -11,7 +21,7 @@
     <title>Calculatrice v3</title>
 </head>
 <body>
-    
+    <form method="POST">
     <table border="1">
         <thead>
             
@@ -20,39 +30,40 @@
             </thead>
             <tr>
                 
-                <td colspan="4"><input type="text" name="" readonly ></td>
+                <td colspan="4"><input type="text" name="text" readonly ></td>
             </tr>
             <tr>
                 <td><input type="submit" name="ac" value="AC"></td>
                 <td><input type="submit" name="C" value="C" ></td>
-                <td colspan="2"><input type="submit" name="" value="<--" ></td>
+                <td colspan="2"><input type="submit" name="delete" value="<--" ></td>
                 
             </tr>
             <tr>
-                <td><input type="submit" name="+" value="+"></td>
+                <td><input type="submit" name="add" value="+"></td>
                 <td><input type="submit" name="1" value="1"></td>
                 <td><input type="submit" name="2" value="2"></td>
                 <td><input type="submit" name="3" value="3"></td>
             </tr>
             <tr>
-                <td><input type="submit" name="-" value="-"></td>
+                <td><input type="submit" name="minus" value="-"></td>
                 <td><input type="submit" name="4" value="4"></td>
                 <td><input type="submit" name="5" value="5"></td>
                 <td><input type="submit" name="6" value="6"></td>
             </tr>
             <tr>
-                <td><input type="submit" name="*" value="*"></td>
+                <td><input type="submit" name="multiplication" value="*"></td>
                 <td><input type="submit" name="7" value="7"></td>
                 <td><input type="submit" name="8" value="8"></td>
                 <td><input type="submit" name="9" value="9"></td>
             </tr>
             <tr>
-                <td><input type="submit" name="/" value="/"></td>
-                <td><input type="submit" name="." value="."></td>
+                <td><input type="submit" name="divition" value="/"></td>
+                <td><input type="submit" name="," value="."></td>
                 <td><input type="submit" name="0" value="0"></td>
-                <td><input type="submit" name="=" value="="></td>
+                <td><input type="submit" name="equal" value="="></td>
             </tr>
         </tbody>
-    </table>    
+    </table> 
+    </form>
 </body>
 </html>
