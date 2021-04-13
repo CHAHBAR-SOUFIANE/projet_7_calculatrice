0a6e9a5e4f6a2b4957725943a4195239ac3be796
CHAHBAR-SOUFIANE <chahbar.soufiane.dev@gmail.com>
2021-04-13 12:48:38 +0000

    .

---
+++ Prototype_initialisation/index.php
@@ -0,0 +1,58 @@
+<?php 
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Calculatrice v3</title>
+</head>
+<body>
+    
+    <table border="1">
+        <thead>
+            
+        <tbody>
+            <tr><td colspan="4" class="titre">Calcularice</td></tr>
+            </thead>
+            <tr>
+                
+                <td colspan="4"><input type="text" name="" readonly ></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="ac" value="AC"></td>
+                <td><input type="submit" name="C" value="C" ></td>
+                <td colspan="2"><input type="submit" name="" value="<--" ></td>
+                
+            </tr>
+            <tr>
+                <td><input type="submit" name="+" value="+"></td>
+                <td><input type="submit" name="1" value="1"></td>
+                <td><input type="submit" name="2" value="2"></td>
+                <td><input type="submit" name="3" value="3"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="-" value="-"></td>
+                <td><input type="submit" name="4" value="4"></td>
+                <td><input type="submit" name="5" value="5"></td>
+                <td><input type="submit" name="6" value="6"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="*" value="*"></td>
+                <td><input type="submit" name="7" value="7"></td>
+                <td><input type="submit" name="8" value="8"></td>
+                <td><input type="submit" name="9" value="9"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="/" value="/"></td>
+                <td><input type="submit" name="." value="."></td>
+                <td><input type="submit" name="0" value="0"></td>
+                <td><input type="submit" name="=" value="="></td>
+            </tr>
+        </tbody>
+    </table>    
+</body>
+</html>
